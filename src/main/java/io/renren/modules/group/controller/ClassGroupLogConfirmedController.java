49bdccc368a7aeffcc9ffb1988ebba1a052dc5ee
15701579485@163.com <15701579485@163.com>
2019-07-19 18:26:24 +0800

    修改班组的 回显

--- src/main/java/io/renren/modules/group/controller/ClassGroupLogConfirmedController.java
+++ src/main/java/io/renren/modules/group/controller/ClassGroupLogConfirmedController.java
@@ -1,29 +1,26 @@
 package io.renren.modules.group.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-
 import io.renren.common.utils.OrderUtils;
 import io.renren.common.utils.PageUtils;
 import io.renren.common.utils.R;
 import io.renren.modules.group.entity.ClassGroupLogEntity;
 import io.renren.modules.group.service.ClassGroupLogConfirmedService;
+import io.renren.modules.setting.entity.BaseTurnEntity;
+import io.renren.modules.setting.service.BaseTurnService;
 import io.renren.modules.sys.entity.NewsEntity;
+import io.renren.modules.sys.entity.SysDeptEntity;
 import io.renren.modules.sys.service.NewsService;
+import io.renren.modules.sys.service.SysDeptService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 班组日志(待确认)
@@ -42,7 +39,11 @@ public class ClassGroupLogConfirmedController {
 	@Autowired
 	private ClassGroupLogConfirmedService classGroupLogConfirmedService;
 	
-	
+	@Autowired
+    private SysDeptService sysDeptService;
+
+    @Autowired
+    private BaseTurnService baseTurnService;
     /**
      * 列表
      */
@@ -61,8 +62,14 @@ public class ClassGroupLogConfirmedController {
     @RequestMapping("/info/{classId}")
     @RequiresPermissions("group:classgrouplogconfirmed:info")
     public R info(@PathVariable("classId") Integer classId){
-			ClassGroupLogEntity classGroupLog = classGroupLogConfirmedService.selectById(classId);
+		ClassGroupLogEntity classGroupLog = classGroupLogConfirmedService.selectById(classId);
+		// 获取部门(车间工段) 名称
+		SysDeptEntity sysDeptEntity = sysDeptService.selectById(classGroupLog.getDeptId());
+		classGroupLog.setDeptName(sysDeptEntity.getName());
 
+		BaseTurnEntity baseTurn = baseTurnService.selectById(classGroupLog.getBaseTurnId());
+		classGroupLog.setBaseTurnName(baseTurn.getName());
+		
         return R.ok().put("classgrouplog", classGroupLog);
     }
 
